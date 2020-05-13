f3a22e7ef18f55971907323b91c7972858ab1f18
Biswas Angkan <48616181+BiswasAngkan@users.noreply.github.com>
2020-05-13 14:44:57 +0200

    Add files via upload

--- Function_Call.py
+++ Function_Call.py
@@ -2,31 +2,31 @@
 # 13.05.2020
 # To define & call functions 
 
-def f1():
-	a = 5
-	print(a)
-	f2()
+def f1():						# Define f1 [f1 = My function]
+	a = 5						# a = Variable 
+	print(a) 					# Call print [print = Python's function], a = Variable/Argument/Parameter
+	f2()						# Call f2 [f2 = My function]
 
-def f2():
-	a = 4
-	print(a)
-	f3(a)
-	b = f4(a)
-	print(b)
+def f2():						# Define f2 [f2 = My function]
+	a = 4 						# a = Variable
+	print(a) 					# Call print [print = Python's function], a = Variable/Argument/Parameter
+	f3(a) 						# Call f3 [f3 = My function], a = Variable
+	b = f4(a) 					# a = Variable/Argument/Parameter, b = Variable, Call f4 [f4 = My function]
+	print(b) 					# Call print [print = Python's function], b = Variable/Argument/Parameter
 
-def f3(b):
-	a = 3
-	print(a)
-	a = a + b
-	print(a)
+def f3(b):						# Define f3 [f3 = My function], b = Variable
+	a = 3						# a = Variable
+	print(a) 					# Call print [print = Python's function], a = Variable/Argument/Parameter
+	a = a + b					# a, b = Variables
+	print(a) 					# Call print [print = Python's function], a = Variable/Argument/Parameter
 		
 
-def f4(c):
-	a = 2
-	print(a)	
-	b = a + c
-	print(b)  
+def f4(c):						# Define f4 [f4 = My function], c = Variable
+	a = 2						# a = Variable
+	print(a) 					# Call print [print = Python's function], a = Variable/Argument/Parameter	
+	b = a + c 					# a, b, c = Variables
+	print(b)					# Call print [print = Python's function], b = Variable/Argument/Parameter 
 	
-	return a
+	return a					# a = Variable/Argument/Parameter
 
-f1()
+f1()							# Call f1 [f1 = My function]
