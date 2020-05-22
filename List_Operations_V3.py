b5b34e220d94e241fdd59c655d99b73ac34b7764
Biswas Angkan <48616181+BiswasAngkan@users.noreply.github.com>
2020-05-22 20:49:36 +0600

    Add files via upload

---
+++ List_Operations_V3.py
@@ -0,0 +1,27 @@
+# Angkan Biswas
+# 22.05.2020
+# To perform diffrent kinds of operations on a variable 
+
+'''Membership Operator'''
+x = [1, 2, 3, 4, 5, 6]
+print(x)
+print(2 in x)
+print(7 in x)
+
+'''Logical/Relational Operator'''
+x = True
+print(not x)
+x = [1, 2, 3, 4, 5, 6]
+print(x)
+print(2 not in x)
+print(True or True)
+print(True and False)
+
+'''Comparison Operator'''
+x = [1, 2, 3, 4, 5, 6]
+print([a == 2 for a in x])
+print([a<2 for a in x])
+print([a>2 for a in x])
+print([a!=2 for a in x])
+print([a>=2 for a in x])
+print([a<=2 for a in x])
