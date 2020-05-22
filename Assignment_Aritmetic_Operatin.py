037190ccc37207b260c891ccdcbea700cc032a5b
Biswas Angkan <48616181+BiswasAngkan@users.noreply.github.com>
2020-05-22 21:00:34 +0600

    Add files via upload

---
+++ Assignment_Aritmetic_Operatin.py
@@ -0,0 +1,26 @@
+# Angkan Biswas
+# 22.05.2020
+# To perform assignment & aritmetic operation
+
+'''Assignment Operator'''
+x = 2
+print(x)
+x += 2					# x = x + 2
+print(x)
+y = 2
+y -= 2					# y = y - 2
+print(y)
+x *= 2 					# x = x * 2
+print(x)
+x /= 2					# x = x / 2
+print(x)
+
+'''Aritmetic Operator'''
+x = 3 + 2; print(x)
+x = 3 - 2; print(x)
+x = 3 * 2; print(x)
+x = 3 / 2; print(x)
+x = 3 // 2; print(x)
+x = 3 ++ 2; print(x)
+x = 3 ** 2; print(x)
+x = 3 % 2; print(x)
