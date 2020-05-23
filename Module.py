8c732ab4b4f1f5b9f17ed665736e66c676db7f58
Biswas Angkan <48616181+BiswasAngkan@users.noreply.github.com>
2020-05-23 11:10:29 +0600

    Add files via upload

---
+++ Module.py
@@ -0,0 +1,33 @@
+# Angkan Biswas
+# 23.05.2020
+# To import module function
+
+'''Module 1'''
+import math
+x = 4 
+y = math.sqrt(x)
+print(y)
+
+'''Module 2'''
+from math import sqrt
+x = 6
+y = sqrt(x)
+print(y)
+
+'''Module 3'''
+import math as m
+x = 8
+y = m.sqrt(x)
+print(y)
+
+'''Module 4'''
+from math import sqrt as s
+x = 10
+y = s(x)
+print(y)
+
+'''Module 5'''
+from math import *
+x = 12
+y = sqrt(x)
+print(y)
