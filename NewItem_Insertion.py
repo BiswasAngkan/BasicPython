89defe208eac377a22edfb281f8940d78acd1717
Biswas Angkan <48616181+BiswasAngkan@users.noreply.github.com>
2020-05-26 17:48:38 +0600

    Add files via upload

---
+++ NewItem_Insertion.py
@@ -0,0 +1,31 @@
+# Angkan Biswas
+# 26.5.2020
+# To insert a new item in a list or a dictionary
+
+
+'''	Empty list	'''
+x = []						
+print('x = {}'.format(x))
+
+
+'''	Insert new items in a list	'''
+x.append('Angkan')
+print('x = {}'.format(x))
+
+x.append(23.44444444)
+print('x = {}'.format(x))
+
+
+'''	Empty dictionary	'''
+y = {}
+print('y = {}'.format(y))
+
+'''	Insert new items in a dictionary	'''
+y['a'] = 12
+print('y = {}'.format(y))
+
+y['b'] = ['Angkan', 1, 890, 'Biswas']
+print('y = {}'.format(y))
+
+y['c'] = ('Dhaka', 1215, 0, 9, 'Farmgate')
+print('y = {}'.format(y))
